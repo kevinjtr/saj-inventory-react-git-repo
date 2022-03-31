28d27ceb442d1af6d797962cc3ca7e0c587d7347
Alemany <1544978469121002@mil>
2022-03-31 13:48:07 -0400

    updated by kla
    updated by kla

--- src/components/AppBarHeader.js
+++ src/components/AppBarHeader.js
@@ -16,6 +16,7 @@ function Header(props) {
 	const {user, userIsLoggedIn, userAccess, darkModeBackgroundColor, prefersDarkMode, setPrefersDarkMode} = props;
 	const [showUserDropdown,setShowUserDropdown] = useState(false)
 
+	//,backgroundImage:'url("../../src/img/appbarBackground.jfif")'
 	return (
 		<Route path="/" render={(history) => (
 			<><AppBar className="appbar-main" position="static" elevation={0}>
@@ -27,7 +28,9 @@ function Header(props) {
 						<div style={{fontSize:"0.6rem", color:"rgba(0,0,0,1)", paddingLeft:"5px", fontWeight:"bold",textShadow:"0 0 2px #878787"}}>US Army Corps of Engineers</div>
 						<div style={{fontSize:"0.6rem", letterSpacing:"0.13rem", color:"rgba(75,75,75,1)", paddingLeft:"5px", fontWeight:"bold",textShadow:"0 0 2px #878787"}}>Jacksonville District</div>
 					</div>
-					<div style={{display:"flex", flexDirection:"column", justifyContent:"center",height:"30px",marginTop:"auto",marginBottom:"auto",marginLeft:"5px"}}><p style={{textTransform:"uppercase", letterSpacing:"0.15rem", fontSize:"1.8rem", color:"rgba(50,50,50,1)", fontWeight:"bold",textShadow:"0 0 1px #000",backgroundImage:'url("../../src/img/appbarBackground.jfif")',backgroundClip:'text'}}>Inventory Beta</p></div>
+					<div style={{display:"flex", flexDirection:"column", justifyContent:"center",height:"30px",marginBottom:"auto",marginLeft:"5px", alignContent:'center'}}>
+						<p style={{textTransform:"uppercase", letterSpacing:"0.15rem", fontSize:"1.8rem", color:"rgba(50,50,50,1)", fontWeight:"bold",textShadow:"0 0 1px #000",backgroundClip:'text'}}>Inventory Beta</p>
+					</div>
                 </Grid>
 				{userIsLoggedIn && (
 					<Grid item sx={{display:"flex",justifyContent:"flex-end",zIndex:"2",position:"relative",gap:'10px',alignItems:'center'}} >
@@ -35,10 +38,10 @@ function Header(props) {
 							<BrightnessLowIcon onClick={()=>setPrefersDarkMode(!prefersDarkMode)} style={{color:'#000000',height:'20px',width:'20px'}} />
 						</Button>
 						
-						<button onClick={()=>setShowUserDropdown(true)} style={{border:'0px',backgroundColor:'rgba(0,0,0,0)',outline:'0'}} >
+						<Button onClick={()=>setShowUserDropdown(true)} style={{border:'0px',outline:'0'}} >
 							<AccountCircle style={{color:'rgb(100,100,100)',fontSize:'20px'}} />
 							<ArrowDropDownIcon style={{color:'rgb(100,100,100)',fontSize:'20px'}}/>
-						</button>
+						</Button>
 
 					</Grid>
 				)
